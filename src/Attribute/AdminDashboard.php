cd185147a4c4afd6e9fd112f161f3b84b867c11d
Jorick Pepin <jorick.pepin@gmail.com>
2024-11-26 20:18:23 +0100

    Fix `AdminDashboard` attribute routes type

--- src/Attribute/AdminDashboard.php
+++ src/Attribute/AdminDashboard.php
@@ -9,7 +9,7 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Attribute;
 class AdminDashboard
 {
     public function __construct(
-        /** @var array<string, array{routeName: string, routePath: string}>|null */
+        /** @var array<string, array{routeName?: string, routePath?: string}>|null */
         public ?array $routes = null,
         /** @var class-string[]|null $allowedControllers If defined, only these CRUD controllers will have a route defined for them */
         public ?array $allowedControllers = null,
