c1b694c1890d6f20858802d201cd7199212dc42a
Javier Eguiluz <javier.eguiluz@gmail.com>
2024-11-28 20:54:15 +0100

    Prepare 4.18.0 release

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '4.17.1-DEV';
+    public const VERSION = '4.18.0';
 
     public function build(ContainerBuilder $container): void
     {
