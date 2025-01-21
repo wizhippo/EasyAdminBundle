7ae7911586bf07c639f06761f01fa360ed8f9f94
Daniel Gorgan <danut007ro@gmail.com>
2025-01-22 01:24:47 +0200

    Add default locale to all pretty routes

--- src/Router/AdminRouteGenerator.php
+++ src/Router/AdminRouteGenerator.php
@@ -68,6 +68,7 @@ final class AdminRouteGenerator implements AdminRouteGeneratorInterface
         private CacheItemPoolInterface $cache,
         private Filesystem $filesystem,
         private string $buildDir,
+        private string $defaultLocale,
     ) {
     }
 
@@ -158,6 +159,7 @@ final class AdminRouteGenerator implements AdminRouteGeneratorInterface
                     }
 
                     $defaults = [
+                        '_locale' => $this->defaultLocale,
                         '_controller' => $crudControllerFqcn.'::'.$actionName,
                         EA::ROUTE_CREATED_BY_EASYADMIN => true,
                         EA::DASHBOARD_CONTROLLER_FQCN => $dashboardFqcn,
