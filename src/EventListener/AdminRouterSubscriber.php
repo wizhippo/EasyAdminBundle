1ced25c757efec5df0ca3c01cd5ba031b4eefe6c
Javier Eguiluz <javier.eguiluz@gmail.com>
2025-02-10 19:39:51 +0100

    Add a comment in the code

--- src/EventListener/AdminRouterSubscriber.php
+++ src/EventListener/AdminRouterSubscriber.php
@@ -289,6 +289,9 @@ class AdminRouterSubscriber implements EventSubscriberInterface
     {
         $routeName = $request->query->get(EA::ROUTE_NAME);
         $routeParams = $request->query->all()[EA::ROUTE_PARAMS] ?? [];
+        // the "relative path" URL type is needed to be compatible with applications
+        // running behind a proxy under a permanent subpath prefix (AKA sub-folder,
+        // sub-url or sub-path - using the HTTP header x-forwarded-prefix)
         $url = $this->urlGenerator->generate($routeName, $routeParams, UrlGeneratorInterface::RELATIVE_PATH);
 
         $newRequest = $request->duplicate();
