c72651dcfa13b989c028447c3bc5cff3e1b8b6b8
Javier Eguiluz <javier.eguiluz@gmail.com>
2025-07-05 09:31:38 +0200

    Minor tweaks

--- src/Twig/Component/Option/AlertVariant.php
+++ src/Twig/Component/Option/AlertVariant.php
@@ -4,6 +4,8 @@ namespace EasyCorp\Bundle\EasyAdminBundle\Twig\Component\Option;
 
 enum AlertVariant: string
 {
+    // these match the variants of Bootstrap 5.3 alerts
+    // see https://getbootstrap.com/docs/5.3/components/alerts/
     case Primary = 'primary';
     case Secondary = 'secondary';
     case Success = 'success';
@@ -12,15 +14,17 @@ enum AlertVariant: string
     case Info = 'info';
     case Light = 'light';
     case Dark = 'dark';
-    case Notice = 'notice'; // commonly used according to https://symfony.com/doc/current/session.html#flash-messages
-    case Error = 'error'; // commonly used according to https://symfony.com/doc/current/session.html#flash-messages
+    // these are not part of Bootstrap, but are commonly used in Symfony applications
+    // see https://symfony.com/doc/current/session.html#flash-messages
+    case Notice = 'notice';
+    case Error = 'error';
 
     public function asBootstrapCssClass(): string
     {
         return 'alert-'.match ($this) {
-            self::Primary, self::Secondary, self::Success, self::Danger, self::Warning, self::Info, self::Light, self::Dark => $this->value,
             self::Notice => 'info',
             self::Error => 'danger',
+            default => $this->value,
         };
     }
 }
