c88165107778efea27f0149cecb41ea5d8a89267
Javier Eguiluz <javier.eguiluz@gmail.com>
2025-10-10 20:26:33 +0200

    Minor tweaks

--- src/Form/Filter/Type/ArrayFilterType.php
+++ src/Form/Filter/Type/ArrayFilterType.php
@@ -27,11 +27,7 @@ class ArrayFilterType extends AbstractType
             static fn ($data) => $data,
             static function ($data) {
                 if (null === $data['value'] || [] === $data['value']) {
-                    $data['comparison'] = \in_array(
-                        $data['comparison'],
-                        [ComparisonType::CONTAINS, ComparisonType::CONTAINS_ALL],
-                        true
-                    ) ? 'IS NULL' : 'IS NOT NULL';
+                    $data['comparison'] = \in_array($data['comparison'], [ComparisonType::CONTAINS, ComparisonType::CONTAINS_ALL], true) ? 'IS NULL' : 'IS NOT NULL';
                 } else {
                     $data['value'] = (array) $data['value'];
                 }
